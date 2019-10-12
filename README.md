67239333808dedb4809b7424eadc9151622d914e
Aditya <mailingadityavs@gmail.com>
2019-10-13 04:21:03 +0530

    First Upload

---
+++ README.md
@@ -0,0 +1,15 @@
+This program will take any C program and convert it to Psuedo code Algorithm with almost 70% accuracy.
+
+It is still under development.
+
+Usage :
+
+algo.py [filename]
+
+Where filename is an optional parameter.
+
+If filename is not given, code copied in clipboard is taken as input and algorithm is generated.
+
+Output :
+
+Algorithm is printed out and copied to clipboard using [pyperclip]
---
+++ algo.py
@@ -0,0 +1,113 @@
+#! python3
+#C Algorithm Generator
+
+import sys
+import os
+import re
+import pyperclip
+
+if(len(sys.argv)<2):
+    text = pyperclip.paste()
+else:
+    fileName = sys.argv[1]
+    text = ""
+
+lines = text.split('\n')
+indentCount = -1
+tab = "\t"
+c = 0
+
+algo = []
+stack = []
+
+for l in lines:
+    lines[c] = l.strip()
+    c = c + 1
+
+
+algo.append("Start")
+
+for l in lines:
+    t = ''
+    if("void" in l):
+        stack.append("Stop")
+        continue
+    elif("int " in l):
+        if(';' in l):
+            t  = "Set Variables"
+    elif(" = " in l):
+            t  = l.replace('=',"<--")
+            t  = t.replace(';',"")
+    elif(l =='{'):
+        indentCount = indentCount + 1
+        continue
+    elif(l == '}'):
+        indentCount = indentCount - 1
+        t = stack[-1]
+        stack.pop()    
+    elif("scanf" in l):
+        t = "Read "
+    elif("printf" in l):
+        t = "Print "
+    elif("else" in l):
+        t = l.replace('if',"If")
+        t = t.replace('else',"Else")
+        stack.append("EndIf.")
+    elif("if(" in l):
+        t = l.replace('if',"If")
+        stack.append("EndIf.")
+    elif("for" in l):
+        t = "While..."
+        stack.append("EndWhile.")
+    elif("while" in l):
+        t = l.replace('while',"While")
+        stack.append("EndWhile.")
+    else:
+        t = l
+
+    algo.append((tab*indentCount)+t)
+
+
+algorithm = "\n".join(algo)
+algorithm = algorithm.replace('('," ")
+algorithm = algorithm.replace(')'," ")
+algorithm = algorithm.replace('%',' MOD ')
+algorithm = algorithm.replace('==','=')
+algorithm = algorithm.replace('&&',' AND ')
+algorithm = algorithm.replace("EndIf.\nElse","Else")
+
+
+print(text,"\n")
+print(algorithm)
+
+pyperclip.copy(algorithm)
+
+# Example Input
+"""
+void main()
+{
+    int a = 5;
+    a = 6;
+    b = a % 3;
+    if(a%5 == 0)
+    {
+        printf("");
+        printf("");
+        while(a<4)
+        {
+            printf("");
+            scanf("");
+        }
+    }
+    else if(a == 0)
+    {
+        scanf("");
+    }
+    else
+    {
+        printf("");
+    }
+    scanf("");
+}
+
+"""
